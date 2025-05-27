cbbcec72e870c41ef0bded6bec35344f245dd9fe
thihasM <thihas2004@gmail.com>
2025-05-27 19:24:37 +0530

    Log In design 5/27/2025 -Thihas

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace LOGIN_SDAM_ASSIGNMENT
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Handiyen());
         }
     }
 }
