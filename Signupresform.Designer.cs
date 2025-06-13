231414c766d555e44658b2dd3b2dd4c63ee5078e
Ganidu <ganiduudage@gmail.com>
2025-06-13 11:35:44 +0530

    Done Signupresform UI design by Ganidu 13/06/2025

--- Signupresform.Designer.cs
+++ Signupresform.Designer.cs
@@ -42,18 +42,20 @@
             sign_res_conpass_txtb = new TextBox();
             signup_res_btn = new Button();
             signup_res_bck_btn = new Button();
-            label2 = new Label();
             sign_res_name_txtb = new TextBox();
             label8 = new Label();
+            label9 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(249, 122);
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(159, 141);
             label1.Name = "label1";
-            label1.Size = new Size(121, 23);
+            label1.Size = new Size(114, 22);
             label1.TabIndex = 0;
             label1.Text = "User Name : ";
             label1.Click += label1_Click;
@@ -61,124 +63,125 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(249, 164);
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(159, 183);
             label3.Name = "label3";
-            label3.Size = new Size(144, 23);
+            label3.Size = new Size(134, 22);
             label3.TabIndex = 2;
             label3.Text = "Enter address : ";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(249, 202);
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(159, 221);
             label4.Name = "label4";
-            label4.Size = new Size(75, 23);
+            label4.Size = new Size(73, 22);
             label4.TabIndex = 3;
             label4.Text = "Email : ";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(249, 244);
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(159, 263);
             label5.Name = "label5";
-            label5.Size = new Size(151, 23);
+            label5.Size = new Size(142, 22);
             label5.TabIndex = 4;
             label5.Text = "Phone Number : ";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(249, 284);
+            label6.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(159, 303);
             label6.Name = "label6";
-            label6.Size = new Size(107, 23);
+            label6.Size = new Size(99, 22);
             label6.TabIndex = 5;
-            label6.Text = "Password ; ";
+            label6.Text = "Password :";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(249, 324);
+            label7.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(159, 343);
             label7.Name = "label7";
-            label7.Size = new Size(181, 23);
+            label7.Size = new Size(174, 22);
             label7.TabIndex = 6;
             label7.Text = "Confirm Password : ";
             // 
             // sign_res_uname_txtb
             // 
-            sign_res_uname_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_uname_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_uname_txtb.BorderStyle = BorderStyle.FixedSingle;
             sign_res_uname_txtb.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sign_res_uname_txtb.Location = new Point(462, 119);
+            sign_res_uname_txtb.Location = new Point(372, 138);
             sign_res_uname_txtb.Name = "sign_res_uname_txtb";
             sign_res_uname_txtb.PlaceholderText = "Enter User Name";
-            sign_res_uname_txtb.Size = new Size(160, 27);
+            sign_res_uname_txtb.Size = new Size(250, 27);
             sign_res_uname_txtb.TabIndex = 7;
             // 
             // sign_res_pass_txtb
             // 
-            sign_res_pass_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_pass_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_pass_txtb.BorderStyle = BorderStyle.FixedSingle;
             sign_res_pass_txtb.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sign_res_pass_txtb.Location = new Point(462, 284);
+            sign_res_pass_txtb.Location = new Point(372, 303);
             sign_res_pass_txtb.Name = "sign_res_pass_txtb";
             sign_res_pass_txtb.PlaceholderText = "Password";
-            sign_res_pass_txtb.Size = new Size(160, 27);
+            sign_res_pass_txtb.Size = new Size(250, 27);
             sign_res_pass_txtb.TabIndex = 8;
             // 
             // sign_res_pnum_txtb
             // 
-            sign_res_pnum_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_pnum_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_pnum_txtb.BorderStyle = BorderStyle.FixedSingle;
-            sign_res_pnum_txtb.Location = new Point(462, 244);
+            sign_res_pnum_txtb.Location = new Point(372, 263);
             sign_res_pnum_txtb.Name = "sign_res_pnum_txtb";
             sign_res_pnum_txtb.PlaceholderText = "+94 XXXXXXXXX";
-            sign_res_pnum_txtb.Size = new Size(160, 27);
+            sign_res_pnum_txtb.Size = new Size(250, 27);
             sign_res_pnum_txtb.TabIndex = 9;
             // 
             // sign_res_email_txtb
             // 
-            sign_res_email_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_email_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_email_txtb.BorderStyle = BorderStyle.FixedSingle;
             sign_res_email_txtb.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sign_res_email_txtb.Location = new Point(462, 202);
+            sign_res_email_txtb.Location = new Point(372, 221);
             sign_res_email_txtb.Name = "sign_res_email_txtb";
             sign_res_email_txtb.PlaceholderText = "Enter Your Email";
-            sign_res_email_txtb.Size = new Size(160, 27);
+            sign_res_email_txtb.Size = new Size(250, 27);
             sign_res_email_txtb.TabIndex = 10;
             // 
             // sign_res_add_txtb
             // 
-            sign_res_add_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_add_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_add_txtb.BorderStyle = BorderStyle.FixedSingle;
             sign_res_add_txtb.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sign_res_add_txtb.Location = new Point(462, 164);
+            sign_res_add_txtb.Location = new Point(372, 183);
             sign_res_add_txtb.Name = "sign_res_add_txtb";
             sign_res_add_txtb.PlaceholderText = "Enter Your Address";
-            sign_res_add_txtb.Size = new Size(160, 27);
+            sign_res_add_txtb.Size = new Size(250, 27);
             sign_res_add_txtb.TabIndex = 11;
             // 
             // sign_res_conpass_txtb
             // 
-            sign_res_conpass_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_conpass_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_conpass_txtb.BorderStyle = BorderStyle.FixedSingle;
             sign_res_conpass_txtb.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sign_res_conpass_txtb.Location = new Point(462, 324);
+            sign_res_conpass_txtb.Location = new Point(372, 343);
             sign_res_conpass_txtb.Name = "sign_res_conpass_txtb";
             sign_res_conpass_txtb.PlaceholderText = "Confirm Password";
-            sign_res_conpass_txtb.Size = new Size(160, 27);
+            sign_res_conpass_txtb.Size = new Size(250, 27);
             sign_res_conpass_txtb.TabIndex = 12;
             // 
             // signup_res_btn
             // 
             signup_res_btn.BackColor = SystemColors.ActiveCaptionText;
+            signup_res_btn.FlatStyle = FlatStyle.Popup;
             signup_res_btn.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             signup_res_btn.ForeColor = SystemColors.ButtonHighlight;
-            signup_res_btn.Location = new Point(476, 386);
+            signup_res_btn.Location = new Point(520, 386);
             signup_res_btn.Name = "signup_res_btn";
             signup_res_btn.Size = new Size(102, 40);
             signup_res_btn.TabIndex = 13;
@@ -189,9 +192,10 @@
             // signup_res_bck_btn
             // 
             signup_res_bck_btn.BackColor = SystemColors.ActiveCaptionText;
+            signup_res_bck_btn.FlatStyle = FlatStyle.Popup;
             signup_res_bck_btn.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             signup_res_bck_btn.ForeColor = SystemColors.ButtonHighlight;
-            signup_res_bck_btn.Location = new Point(298, 386);
+            signup_res_bck_btn.Location = new Point(414, 386);
             signup_res_bck_btn.Name = "signup_res_bck_btn";
             signup_res_bck_btn.RightToLeft = RightToLeft.No;
             signup_res_bck_btn.Size = new Size(102, 40);
@@ -200,46 +204,58 @@
             signup_res_bck_btn.UseVisualStyleBackColor = false;
             signup_res_bck_btn.Click += signup_res_bck_btn_Click;
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(313, 39);
-            label2.Name = "label2";
-            label2.Size = new Size(157, 38);
-            label2.TabIndex = 15;
-            label2.Text = "Handiyen";
-            // 
             // sign_res_name_txtb
             // 
-            sign_res_name_txtb.BackColor = SystemColors.ScrollBar;
+            sign_res_name_txtb.BackColor = SystemColors.ControlLightLight;
             sign_res_name_txtb.BorderStyle = BorderStyle.FixedSingle;
             sign_res_name_txtb.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sign_res_name_txtb.Location = new Point(462, 80);
+            sign_res_name_txtb.Location = new Point(372, 99);
             sign_res_name_txtb.Name = "sign_res_name_txtb";
             sign_res_name_txtb.PlaceholderText = "Enter Resturant Name";
-            sign_res_name_txtb.Size = new Size(160, 27);
+            sign_res_name_txtb.Size = new Size(250, 27);
             sign_res_name_txtb.TabIndex = 17;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(249, 83);
+            label8.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(159, 102);
             label8.Name = "label8";
-            label8.Size = new Size(76, 23);
+            label8.Size = new Size(72, 22);
             label8.TabIndex = 16;
             label8.Text = "Name : ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Stencil", 28.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label9.Location = new Point(262, 25);
+            label9.Name = "label9";
+            label9.Size = new Size(257, 56);
+            label9.TabIndex = 31;
+            label9.Text = "Handiyen";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.PowderBlue;
+            pictureBox1.Image = Properties.Resources.d;
+            pictureBox1.Location = new Point(689, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(99, 89);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 36;
+            pictureBox1.TabStop = false;
             // 
             // Signupresform
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ButtonShadow;
+            BackColor = Color.PowderBlue;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label9);
             Controls.Add(sign_res_name_txtb);
             Controls.Add(label8);
-            Controls.Add(label2);
             Controls.Add(signup_res_bck_btn);
             Controls.Add(signup_res_btn);
             Controls.Add(sign_res_conpass_txtb);
@@ -256,6 +272,7 @@
             Controls.Add(label1);
             Name = "Signupresform";
             Text = "Signupresform";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -276,8 +293,9 @@
         private TextBox sign_res_conpass_txtb;
         private Button signup_res_btn;
         private Button signup_res_bck_btn;
-        private Label label2;
         private TextBox sign_res_name_txtb;
         private Label label8;
+        private Label label9;
+        private PictureBox pictureBox1;
     }
 }
